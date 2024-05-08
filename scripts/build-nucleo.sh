c8689550a51ae50bafb1fb9cd81cf1b4da090c02
sgabre <godefroy.sieffert>
2024-05-08 09:25:57 +0200

    Add blinker firmware

--- scripts/build-nucleo.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-./build.sh -p nucleo -i ../binary -b ../build -c -r -t arm-none-eabi-gcc
--- scripts/path.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-export PATH="$PATH:/Applications/ArmGNUToolchain/13.2.Rel1/arm-none-eabi/bin/"
