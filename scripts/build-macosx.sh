d1ff6949ee31cecb0b36b3d83d6a2fdb57664a2f
sgabre <godefroy.sieffert>
2024-05-08 09:58:52 +0200

    add scripts

---
+++ scripts/build-macosx.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+./build.sh -p macosx -i ../binary -b ../build -c -r
+    
---
+++ scripts/build-nucleo.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+./build.sh -p nucleo -i ../binary -b ../build -c -r -t arm-none-eabi-gcc
